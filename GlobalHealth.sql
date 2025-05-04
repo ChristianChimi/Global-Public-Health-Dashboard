f90eee4d84a58290a9cdbcaa943681ff81f17f8f
Christian christian <75487027+ChristianChimi@users.noreply.github.com>
2025-05-04 13:15:57 +0200

    Create GlobalHealth.sql

---
+++ GlobalHealth.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE VIEW GlobalHealth AS
+SELECT 
+    "DTP3"."Entity", 
+    "DTP3"."Year",
+    "DTP3"."DTP3",
+    "POL3"."POL3",
+    "ChildMortalityRate"."Child Mortality Rate",
+    "LifeExpectancy"."Life Expectancy"
+FROM 
+    "DTP3"
+INNER JOIN 
+    "POL3"
+ON 
+    "POL3"."Year" = "DTP3"."Year"
+    AND "DTP3"."Entity" = "POL3"."Entity"
+INNER JOIN 
+    "ChildMortalityRate"
+ON 
+    "DTP3"."Year" = "ChildMortalityRate"."Year"
+    AND "DTP3"."Entity" = "ChildMortalityRate"."Entity"
+INNER JOIN 
+    "LifeExpectancy"
+ON 
+    "DTP3"."Year" = "LifeExpectancy"."Year"
+    AND "DTP3"."Entity" = "LifeExpectancy"."Entity";
+
+SELECT 
+    "Entity" AS "Country",
+    "Year",
+    "DTP3" as "DTP3_Share",
+    "POL3" as "POL3_Share",
+    "Child Mortality Rate" as "Child_Mortality_Rate", 
+    "Life Expectancy" as "Life_Expectancy"
+FROM globalhealth;
